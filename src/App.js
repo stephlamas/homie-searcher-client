b2939d970854de583e599ede9886ec8679183ba1
Shirley Gianina <shirley.cd853f@Shirleys-MacBook-Pro.local>
2022-03-10 17:49:50 +0100

    solved deploy error again 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import "./App.css";
+import "./styles/App.css";
 import Navigation from "./components/Navigation/Navigation";
 import AppRoutes from "./routes/AppRoutes";
 import Footer from "./components/Footer/Footer";
 import UserMessage from "./components/UserMessage/UserMessage";
-
 
 function App() {
   return (
